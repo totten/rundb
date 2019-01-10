ba18359bbcdcc4c09a7126eeb84758457981906e
Tim Otten <totten@civicrm.org>
2019-01-09 23:57:50 -0800

    Initial import

---
+++ default.nix
@@ -0,0 +1,35 @@
+let
+
+  pkgs = import (fetchTarball https://github.com/NixOS/nixpkgs-channels/archive/nixos-18.09.tar.gz) {};
+  stdenv = pkgs.stdenv;
+
+in rec {
+
+  master = stdenv.mkDerivation rec {
+    name = "2db-master";
+    bin = ./bin;
+    buildInputs = [ pkgs.mariadb ];
+    buildCommand = ''
+    '';
+    shellHook = ''
+      export PATH="$bin:$PATH"
+      export MYSQL_BASE="$PWD/master"
+      export MYSQL_HOME="$MYSQL_BASE/conf"
+    '';
+    };
+
+
+  slave =  stdenv.mkDerivation rec {
+    name = "2db-slave";
+    bin = ./bin;
+    buildInputs = [ pkgs.mariadb ];
+    buildCommand = ''
+    '';
+    shellHook = ''
+      export PATH="$bin:$PATH"
+      export MYSQL_BASE="$PWD/slave"
+      export MYSQL_HOME="$MYSQL_BASE/conf"
+    '';
+    };
+
+}
