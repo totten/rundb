514e3072c7efbf64fdd98c2d36fea2de6673a8b0
Tim Otten <totten@civicrm.org>
2019-01-10 00:51:45 -0800

    default.nix - don't repeat yourself

--- default.nix
+++ default.nix
@@ -3,33 +3,22 @@ let
   pkgs = import (fetchTarball https://github.com/NixOS/nixpkgs-channels/archive/nixos-18.09.tar.gz) {};
   stdenv = pkgs.stdenv;
 
+  mkdbms = {dbmsName}: stdenv.mkDerivation rec {
+    name = "rundb-${dbmsName}";
+    bin = ./bin;
+    buildInputs = [ pkgs.mariadb ];
+    buildCommand = ''
+    '';
+    shellHook = ''
+      export PATH="$bin:$PATH"
+      export MYSQL_BASE="$PWD/'' + dbmsName + ''"
+      export MYSQL_HOME="$MYSQL_BASE/conf"
+    '';
+    };
+
 in rec {
 
-  master = stdenv.mkDerivation rec {
-    name = "rundb-master";
-    bin = ./bin;
-    buildInputs = [ pkgs.mariadb ];
-    buildCommand = ''
-    '';
-    shellHook = ''
-      export PATH="$bin:$PATH"
-      export MYSQL_BASE="$PWD/master"
-      export MYSQL_HOME="$MYSQL_BASE/conf"
-    '';
-    };
-
-
-  slave =  stdenv.mkDerivation rec {
-    name = "rundb-slave";
-    bin = ./bin;
-    buildInputs = [ pkgs.mariadb ];
-    buildCommand = ''
-    '';
-    shellHook = ''
-      export PATH="$bin:$PATH"
-      export MYSQL_BASE="$PWD/slave"
-      export MYSQL_HOME="$MYSQL_BASE/conf"
-    '';
-    };
+  master = mkdbms { dbmsName = "master"; };
+  slave = mkdbms { dbmsName = "slave"; };
 
 }
